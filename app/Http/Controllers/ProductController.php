5bbf7240099c4f13dc8fda1e0ffdd12f790539c4
sebibaciu <sebibaciu@gmail.com>
2022-04-08 00:34:43 +0300

    implementat get/set product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -8,12 +9,12 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     //produse dummy pentru baza de date
-//    public static $products = [
-//        ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "image" => "game.png", "price"=>"1000"],
-//        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "image" => "safe.png", "price"=>"999"],
-//        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "image" => "submarine.png", "price"=>"30"],
-//       ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "image" => "game.png", "price"=>"100"]
-//   ];
+    //    public static $products = [
+    //        ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "image" => "game.png", "price"=>"1000"],
+    //        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "image" => "safe.png", "price"=>"999"],
+    //        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "image" => "submarine.png", "price"=>"30"],
+    //       ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "image" => "game.png", "price"=>"100"]
+    //   ];
 
     //ia arrayul si il trimite la product.index pentru display
     public function index()
@@ -30,8 +31,8 @@ class ProductController extends Controller
     {
         $viewData = [];
         $product = Product::findOrFail($id);
-        $viewData["title"] = $product["name"]." - Online Store";
-        $viewData["subtitle"] =  $product["name"]." - Product information";
+        $viewData["title"] = $product->getName()." - Online Store";
+        $viewData["subtitle"] =  $product->getName()." - Product information";
         $viewData["product"] = $product;
         return view('product.show')->with("viewData", $viewData);
     }
--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
-@section('title', $viewData["title"])
-@section('subtitle', $viewData["subtitle"])
+@section('title', $viewData['title'])
+@section('subtitle', $viewData['subtitle'])
 @section('content')
-<div class="container">
-  <div class="row">
-    <div class="col-lg-4 ms-auto">
-      <p class="lead">{{ $viewData["description"] }}</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 ms-auto">
+                <p class="lead">{{ $viewData['description'] }}</p>
+            </div>
+            <div class="col-lg-4 me-auto">
+                <p class="lead">{{ $viewData['author'] }}</p>
+            </div>
+        </div>
     </div>
-    <div class="col-lg-4 me-auto">
-      <p class="lead">{{ $viewData["author"] }}</p>
-    </div>
-  </div>
-</div>
 @endsection
