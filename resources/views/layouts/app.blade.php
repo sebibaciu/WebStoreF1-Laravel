0fcce4c43d56e6e766bcb8306df5180a21892a11
sebibaciu <sebibaciu@gmail.com>
2022-04-09 23:14:34 +0300

    Incepere implementare panou admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,10 +42,7 @@
   <div class="copyright py-4 text-center text-white">
     <div class="container">
       <small>
-        Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank"
-          href="https://twitter.com/danielgarax">
-          Daniel Correa
-        </a> - <b>Paola Vallejo</b>
+        Copyright<b>
       </small>
     </div>
   </div>
